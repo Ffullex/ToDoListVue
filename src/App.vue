16d0a0c8499eace28465c399b7be8c1454cc750e
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-05-13 10:11:27 +0700

    Begin

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ToDoForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ToDoForm from './components/ToDoForm'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoForm
   }
 }
 </script>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,15 @@
+<template>
+<form>
+  <input type="text" name="area" value="MeoW ^^">
+</form>
+</template>
+
+<script>
+export default {
+  name: "ToDoForm",
+}
+</script>
+
+<style scoped>
+
+</style>
